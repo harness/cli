5aaaeed0b43191dc42ac7e7a81deb1444a153a38
Mike Sawka <mike.sawka@harness.io>
2026-07-30 10:18:51 -0700

    update local-setup.zsh to set HARNESS_CLI_HOME and check pwd

--- local-setup.zsh
+++ local-setup.zsh
@@ -1,4 +1,10 @@
+if [[ ! -f "$(pwd)/go.mod" ]] || ! grep -q '^module github.com/harness/cli$' "$(pwd)/go.mod"; then
+  echo "local-setup.zsh must be sourced from the root of the harness CLI repo" >&2
+  return 1
+fi
+
 if [[ ":$PATH:" != *":$(pwd)/bin:"* ]]; then
   export PATH="$(pwd)/bin:$PATH"
 fi
+export HARNESS_CLI_HOME="$(pwd)/devhome"
 source <(harness completion zsh)
